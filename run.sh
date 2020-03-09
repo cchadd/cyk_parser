47009808fce67f873d62d821c599c0009219f89f
cchadd <clement.chadebec@mines-paristech.fr>
2020-03-09 19:16:44 +0100

    cyk_parser

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python script/main.py -s test_sentence.txt -p parser/cyk_parser -f parsed_sentences -t
+python script/main.py -s data/test_sentence.txt -p parser/cyk_parser -f data/parsed_sentences -t
